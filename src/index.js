8c186161771904665b117aacd691b1688070c6c7
Andraz Vrhovec <andraz@koofr.net>
2022-11-06 23:30:23 +0100

    wip

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Container className="p-3">
+<Alert variant='primary'>
+    hahah
+</Alert>
+</Container>, document.getElementById('root'));
